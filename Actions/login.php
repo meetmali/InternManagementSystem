d88606cf957db3398c519d37d322bd3f8cb80979
DHANRAJ BHARAT PANCHAL <48233707+Dhanraj01@users.noreply.github.com>
2023-08-01 17:01:06 +0530

    created my profile page

--- Actions/login.php
+++ Actions/login.php
@@ -1,12 +1,22 @@
 <?php
 include('connect.php');
-// session_start();
+session_start();
+if (isset($_SESSION['user_id'])) {
+    header("Location: ../Partials/myprofile.php");
+    exit();
+  }
+$_SESSION['LAST_ACTIVITY'] = time();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
 $query = "select * from interns where intern_email = '$username' and intern_password = '$password';";
 $result = $conn->query($query);
 if ($result->num_rows > 0) {
+    // Fetch intern data and store it in the session
+    $internData = $result->fetch_assoc();
+    $internID = $internData["intern_id"];
+    $_SESSION['user_id'] = $internID;
+    $_SESSION['intern_data'] = $internData;
 	echo "<table>";
     echo "<tr><th>Column 1</th><th>Column 2</th><th>Column 3</th></tr>";
     
@@ -19,9 +29,12 @@ if ($result->num_rows > 0) {
         echo "<td>" . $row["intern_password"] . "</td>";
         echo "<td>" . $row["intern_email"] . "</td>";
         echo "</tr>";
+
     }
+
     
     echo "</table>";
+    echo "<a href=\"../Partials/myprofile.php\">My Profile</a>";
 }
 else {
     echo "Login Unsuccessful";
